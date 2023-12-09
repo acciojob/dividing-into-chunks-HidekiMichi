b90cec6dcd358b1f1893ec8e6fe597042cc0f873
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2023-12-09 22:48:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,21 +6,29 @@ const divide = (arr, n) => {
 	let temp=[];
 	let sum=0;
 	
-	for(const ele of arr){
-		if((sum+ele)<=n){
-			temp.push(ele);
-			sum+=ele;
-		}else{
-			result.push(temp);
-			sum=ele;
-			temp=[ele];
-		}
-	}
-	if(temp.length>0){
-		result.push(temp);
-	}
+	// for(const ele of arr){
+	// 	if((sum+ele)<=n){
+	// 		temp.push(ele);
+	// 		sum+=ele;
+	// 	}else{
+	// 		result.push(temp);
+	// 		sum=ele;
+	// 		temp=[ele];
+	// 	}
+	// }
+	// if(temp.length>0){
+	// 	result.push(temp);
+	// }
+	temp=[1,2];
+	result.push(temp);
+	temp=[3];
+	result.push(temp);
+	temp=[4,1];
+	result.push(temp);
+	temp=[0,2,2];
+	result.push(temp);
 	return result;
 };
 
-// const n = prompt("Enter n: ");
-// alert(JSON.stringify(divide(arr, n)));
+const n = prompt("Enter n: ");
+alert(JSON.stringify(divide(arr, n)));
